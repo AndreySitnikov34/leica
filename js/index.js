65ba8d08fa02defacb35616368e43193ed35a1db
AndreySitnikov34 <ac1970@yandex.ru>
2022-01-29 16:56:28 +0300

    after check

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ btnRepair.innerHTML = ('Круто, спасибо за доверие!'); // З
 });
 })
 
+const mediaQuery = window.matchMedia('(max-width: 525px)');
+if (mediaQuery.matches) {
+  btnRepairs.forEach(btnRepair => {// Для каждой кнопки
+btnRepair.addEventListener("click", function(event) {// По клику вызываем функцию "событие"
+event.preventDefault();// Чтоб не открылось в новом окне (отм. действия браузера по умолчанию)
+btnRepair.innerHTML = ('👍'); // Заменяем содержимое кнопки
+});
+})
+}
